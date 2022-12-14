430443576de26d779a71c6955ffeaab4444385eb
Andre Volmensky <andre@volmens.ky>
2022-12-14 09:29:52 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='panasoniceolia',
-    version='0.0.1',
+    version='0.0.2',
     description='Read and change status of Panasonic Eolia devices',
     long_description=long_description,
     long_description_content_type="text/markdown",
